a27962b6f858fcc8c9563058bea527a9de98ab54
Moroine Bentefrit <moroine.bentefrit@beta.gouv.fr>
2024-04-23 16:21:35 +0200

    chore: remove useless files

--- .bin/commands.sh
+++ .bin/commands.sh
@@ -6,17 +6,10 @@ function Help() {
    # Display Help
    echo "Commands"
    echo "  bin:setup                                               Installs ${PRODUCT_NAME} binary with zsh completion on system"
-   echo "  init:env                                                Update local env files using values from vault file"
-   echo "  release:interactive                                                                Build & Push Docker image releases"
-   echo "  release:app                                                                Build & Push Docker image releases"
    echo "  deploy <env> --user <your_username>                                           Deploy application to <env>"
-   echo "  preview:build                                                                Build preview"
-   echo "  preview:cleanup --user <your_username>                                        Remove preview from close pull-requests"
    echo "  vault:init                                                                    Fetch initial vault-password from template-apprentissage"
    echo "  vault:edit                                                                    Edit vault file"
    echo "  vault:password                                                                Show vault password"
-   echo "  seed:update                                Update seed using a database"
-   echo "  seed:apply                                 Apply seed to a database"
    echo "  deploy:log:encrypt                         Encrypt Github ansible logs"
    echo "  deploy:log:dencrypt                        Decrypt Github ansible logs"
    echo 
@@ -31,28 +24,8 @@ function bin:setup() {
   sudo rm -f ~/.zcompdump*
 }
 
-function init:env() {
-  "${SCRIPT_DIR}/setup-local-env.sh" "$@"
-}
-
-function release:interactive() {
-  "${SCRIPT_DIR}/release-interactive.sh" "$@"
-}
-
-function release:app() {
-  "${SCRIPT_DIR}/release-app.sh" "$@"
-}
-
 function deploy() {
   "${SCRIPT_DIR}/deploy-app.sh" "$@"
-}
-
-function preview:build() {
-  "${SCRIPT_DIR}/build-images.sh" "$@"
-}
-
-function preview:cleanup() {
-  "${SCRIPT_DIR}/run-playbook.sh" "preview_cleanup.yml" "preview"
 }
 
 function vault:init() {
@@ -68,14 +41,6 @@ function vault:edit() {
 
 function vault:password() {
   "${SCRIPT_DIR}/get-vault-password-client.sh" "$@"
-}
-
-function seed:update() {
-  "${SCRIPT_DIR}/seed-update.sh" "$@"
-}
-
-function seed:apply() {
-  "${SCRIPT_DIR}/seed-apply.sh" "$@"
 }
 
 function deploy:log:encrypt() {
--- .bin/product-meta.sh
+++ .bin/product-meta.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 set -euo pipefail
 
-export PRODUCT_NAME=tmpl
-export REPO_NAME=template-apprentissage
-export DATABASE_NAME=tmpl
+export PRODUCT_NAME=mongodb
