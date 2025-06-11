1fc959546a8914dbe9d1b3622382e358eba44ebf
Kevin Barnoin <k.barnoin@gmail.com>
2025-06-11 17:57:13 +0200

    fix: get vault password product name

--- .bin/scripts/get-vault-password-client.sh
+++ .bin/scripts/get-vault-password-client.sh
@@ -12,7 +12,6 @@ readonly BIN_DIR="$(dirname "${SCRIPT_DIR}")"
 readonly ROOT_DIR="$(dirname "${BIN_DIR}")"
 readonly VAULT_DIR="${ROOT_DIR}/.infra/vault"
 readonly VAULT_FILE="${VAULT_DIR}/vault.yml"
-readonly PRODUCT_NAME="tmpl"
 
 DOCUMENT_CONTENT=$(op --account mission-apprentissage document get ".vault-password-${PRODUCT_NAME}" --vault "mna-vault-passwords-common" || echo "")
 vault_password_file="${VAULT_DIR}/.vault-password.gpg"
