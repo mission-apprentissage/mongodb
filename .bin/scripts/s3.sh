24c3c94f886111b31e59db177b7e503f987879a0
mathieu <mathieu.borrell@beta.gouv.fr>
2025-07-25 10:47:47 +0200

    fix: remplacement de la commande rm par shred et correction des résultats renvoyés par yq (#51)
    * fix: suppression de la commande qui tue l'agent GnuPG
    
    * fix: remplacement de la commande rm par shred
    
    * feat: ajout d'un contrôle sur les commandes requises
    
    * fix: correction des résultats renvoyés par yq
    
    * fix: prise en compte du contexte CI sur le contrôle des dépendances

--- .bin/scripts/s3.sh
+++ .bin/scripts/s3.sh
@@ -9,21 +9,22 @@ else
 fi
 
 delete_cleartext() {
-  rm -f "${ROOT_DIR}/.vault_pwd.txt"
+  if [ -f "${ROOT_DIR}/.vault_pwd.txt" ]; then
+    shred -f -n 10 -u "${ROOT_DIR}/.vault_pwd.txt"
+  fi
 }
-
 trap delete_cleartext EXIT
 
 readonly VAULT_FILE="${ROOT_DIR}/.infra/vault/vault.yml"
 
 ansible-vault view "${ansible_extra_opts[@]}" "$VAULT_FILE" > "${ROOT_DIR}/.vault_pwd.txt"
 
-OVH_S3_USER=$(cat "${ROOT_DIR}/.vault_pwd.txt" | yq '.vault.OVH_S3_USER')
-OVH_S3_API_KEY=$(cat "${ROOT_DIR}/.vault_pwd.txt" | yq '.vault.OVH_S3_API_KEY')
-OVH_S3_API_SECRET=$(cat "${ROOT_DIR}/.vault_pwd.txt" | yq '.vault.OVH_S3_API_SECRET')
-OVH_S3_BUCKET=$(cat "${ROOT_DIR}/.vault_pwd.txt" | yq '.vault.OVH_S3_BUCKET')
-OVH_S3_ENDPOINT=$(cat "${ROOT_DIR}/.vault_pwd.txt" | yq '.vault.OVH_S3_ENDPOINT')
-OVH_S3_REGION=$(cat "${ROOT_DIR}/.vault_pwd.txt" | yq '.vault.OVH_S3_REGION')
+OVH_S3_USER=$(cat "${ROOT_DIR}/.vault_pwd.txt" | yq -r '.vault.OVH_S3_USER')
+OVH_S3_API_KEY=$(cat "${ROOT_DIR}/.vault_pwd.txt" | yq -r '.vault.OVH_S3_API_KEY')
+OVH_S3_API_SECRET=$(cat "${ROOT_DIR}/.vault_pwd.txt" | yq -r '.vault.OVH_S3_API_SECRET')
+OVH_S3_BUCKET=$(cat "${ROOT_DIR}/.vault_pwd.txt" | yq -r '.vault.OVH_S3_BUCKET')
+OVH_S3_ENDPOINT=$(cat "${ROOT_DIR}/.vault_pwd.txt" | yq -r '.vault.OVH_S3_ENDPOINT')
+OVH_S3_REGION=$(cat "${ROOT_DIR}/.vault_pwd.txt" | yq -r '.vault.OVH_S3_REGION')
 
 export AWS_ACCESS_KEY_ID="${OVH_S3_API_KEY}"
 export AWS_SECRET_ACCESS_KEY="${OVH_S3_API_SECRET}" 
