709eba955c00af589249384c600866b61e72b6ac
Mathieu Borrell <mathieu.borrell@beta.gouv.fr>
2026-02-20 15:45:08 +0100

    fix

--- .bin/commands.sh
+++ .bin/commands.sh
@@ -35,6 +35,9 @@ _meta_help["app:deploy:cluster:node:update"]="Update cluster"
 
 function app:deploy:cluster:node:update() {
   "${SCRIPT_DIR}/product-validate-env.sh" "$1"
+
+  exit
+
   "${SCRIPT_SHARED_DIR}/app-deploy.sh" "$@" --extra-vars "context=update"
 }
 
--- .bin/scripts/product-validate-env.sh
+++ .bin/scripts/product-validate-env.sh
@@ -9,11 +9,9 @@ ROOT_DIR="${BIN_DIR}/../.."
 
 readonly ENV_IP=$(ANSIBLE_CONFIG="${ROOT_DIR}/.infra/ansible/ansible.cfg" ansible-inventory --list -l "$1" 2>/dev/null | jq -r ".${1}.hosts[0]")
 
-echo $ENV_IP
+if [ "$ENV_IP" == "null" ]; then
 
-if [[ "$ENV_IP" == "null" ]]; then
-
-  if [[ -z "${CI:-}" ]]; then
+  if [ -z "${CI:-}" ]; then
     exit 1
   else
     exit 0
